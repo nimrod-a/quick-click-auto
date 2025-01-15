41ccd349c2a0e382e19f1499e81020cb290e7ab8
Nimrod Adam <nimrod-adam@riseup.net>
2025-01-15 13:40:34 +0100

    Update exception naming

--- quick_click_auto/exceptions.py
+++ quick_click_auto/exceptions.py
@@ -1,28 +1,28 @@
 # This file is part of quick-click-auto.
-# It is largley based on [auto-click-auto](https://github.com/KAUTH/auto-click-auto/)
+# It is based on [auto-click-auto](https://github.com/KAUTH/auto-click-auto/)
 # by Konstantinos Papadopoulos .
 # Licensed under the MIT License.
 
-class AutoClickAutoError(Exception):
-    """Custom exception with an error message for `auto_click_auto`."""
+class QuickClickAutoError(Exception):
+    """Custom exception with an error message for `quick_click_auto`."""
 
     def __init__(self, message):
         super().__init__(message)
 
 
-class ShellConfigurationFileNotFoundError(AutoClickAutoError):
+class ShellConfigurationFileNotFoundError(QuickClickAutoError):
     """Exception raised when the shell configuration file does not exist."""
 
     pass
 
 
-class ShellTypeNotSupportedError(AutoClickAutoError):
+class ShellTypeNotSupportedError(QuickClickAutoError):
     """Exception raised when the shell type is not supported."""
 
     pass
 
 
-class ShellEnvVarNotFoundError(AutoClickAutoError):
+class ShellEnvVarNotFoundError(QuickClickAutoError):
     """
     Exception raised when the shell type cannot be inferred from the `SHELL`
     environment variable.
